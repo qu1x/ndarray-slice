87012418f24bcaba2a273133b8b44496769df610
Rouven Spreckels <rs@qu1x.dev>
2023-05-28 18:28:22 +0200

    Update description selection algorithm.

--- src/lib.rs
+++ src/lib.rs
@@ -1142,8 +1142,9 @@ where
 	///
 	/// # Current Implementation
 	///
-	/// The current algorithm is based on the quickselect portion of the same quicksort algorithm
-	/// used for [`sort_unstable`].
+	/// The current algorithm is an introselect implementation based on Pattern Defeating Quicksort, which is also
+	/// the basis for [`sort_unstable`]. The fallback algorithm is Median of Medians using Tukey's Ninther for
+	/// pivot selection, which guarantees linear runtime for all inputs.
 	///
 	/// [`sort_unstable`]: Slice1Ext::sort_unstable
 	///
@@ -1193,8 +1194,9 @@ where
 	///
 	/// # Current Implementation
 	///
-	/// The current algorithm is based on the quickselect portion of the same quicksort algorithm
-	/// used for [`sort_unstable`].
+	/// The current algorithm is an introselect implementation based on Pattern Defeating Quicksort, which is also
+	/// the basis for [`sort_unstable`]. The fallback algorithm is Median of Medians using Tukey's Ninther for
+	/// pivot selection, which guarantees linear runtime for all inputs.
 	///
 	/// [`sort_unstable`]: Slice1Ext::sort_unstable
 	///
@@ -1245,8 +1247,9 @@ where
 	///
 	/// # Current Implementation
 	///
-	/// The current algorithm is based on the quickselect portion of the same quicksort algorithm
-	/// used for [`sort_unstable`].
+	/// The current algorithm is an introselect implementation based on Pattern Defeating Quicksort, which is also
+	/// the basis for [`sort_unstable`]. The fallback algorithm is Median of Medians using Tukey's Ninther for
+	/// pivot selection, which guarantees linear runtime for all inputs.
 	///
 	/// [`sort_unstable`]: Slice1Ext::sort_unstable
 	///
